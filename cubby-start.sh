b89304297ab6e53f7f58acd395df1aac7cc031d6
Joejoe <mrosata1984@gmail.com>
2017-11-25 20:18:39 -0500

    updated package installd and config backups

--- cubby-start.sh
+++ cubby-start.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "Cubby - your configuration bootstrapper is \
-about to setup this machine for the first time."
-
 # Errors
 ERRNO_NOSUDO=101
 
@@ -13,38 +10,53 @@ CUBBY_CONF_DIR="$PWD/home-configs"
 
 SETUP_GITHUB_CREDS=${SETUP_GITHUB_CREDS:-1}
 
+echo "Cubby - your configuration bootstrapper is \
+about to setup this machine for the first time."
 
-# Configure Git Credentials easily using ./install-git.sh
-[ $SETUP_GITHUB_CREDS -ne 0 ] && ./install-git.sh
 
+# Install packages list in apt-packages.list file using apt-get
+apt_packages_list="$PWD/apt-packages.list"
+[ -f "$apt_packages_list" ] && \
+  cat "$apt_packages_list" | xargs sudo apt-get install -y
+
+
+# Configure Git Credentials easily using "$PWD/install-git.sh"
+[ $SETUP_GITHUB_CREDS -ne 0 ] && "$PWD/install-git.sh"
+
+
+# Copy all cubby config files 
 echo "Creating cubby configs directory"
 [ ! -d "$LOCAL_CUBBY" -o ! -d "$LOCAL_CONF_DIR" ] && \
   mkdir -p "$LOCAL_CONF_DIR"
-cp "${CUBBY_CONF_DIR}"/.* "$LOCAL_CONF_DIR"
+cp -aui "$CUBBY_CONF_DIR/." "$LOCAL_CONF_DIR/"
 
+
+# Setup Vundle and Vim if not already present
 if [ ! -d "$HOME/.vim/bundle/Vundle.vim" ]; then
-  if ./install-vundle.sh ; then
+  if "$PWD/install-vundle.sh" ; then
     echo "[*] - Installed Vundle"
   else
     echo "[x] - Non-zero exit trying to install Vundle"
   fi
 fi
 
-# TODO: Setup the vimrc download
-if [ ! -f "${HOME}/.vimrc" ];then
+
+# Copy .vimrc config from local config folder to home
+if [ ! -f "$HOME/.vimrc" ] ; then
   [ -f "$HOME/.vimrc" ] && \
-    mv "$HOME/.vimrc" "$HOME/.vimrc~bak"
+    cp -b "$HOME/.vimrc" "$HOME/"
   [ -f "$LOCAL_CONF_DIR/.vimrc" ] && \
-    cat "$LOCAL_CONF_DIR/.vimrc" > "${HOME}/.vimrc"
+    cp -u "$LOCAL_CONF_DIR/.vimrc" "${HOME}/"
 fi
 
 
 echo "[*] - About to install docker-ce for debian"
-if ./install-docker.sh ; then
+if "$PWD/install-docker.sh" ; then
   echo "Docker Install Script Exited OK"
 else
   echo "Docker Install Script Exited with non-zero"
 fi
 
 
-exit 0;
+exit 0
+
